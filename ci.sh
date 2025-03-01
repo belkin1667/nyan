b635527bd50a660f4680196e39edc9641e1100b6
Mike Belkin <bel.kin1667@gmail.com>
2025-03-02 02:56:52 +0300

    better

--- ci.sh
+++ ci.sh
@@ -1,4 +1,5 @@
 flake8 nyan/ --count --ignore=C901,E741,W503,PIE786,E203 --show-source --statistics
 flake8 nyan/ --count --exit-zero --max-complexity=10 --statistics
 mypy --strict nyan/
+./download_models.sh
 pytest -s
